aad144d9a6d623a4739338c6c9135422f03f3503
elizapieza <pageantgurlmariah@gmail.com>
2026-05-19 20:02:04 -0400

    data-driven task validation implemented

--- tests/asana.spec.ts
+++ tests/asana.spec.ts
@@ -1,4 +1,5 @@
 import { expect, Page, test } from '@playwright/test';
+import { taskCases } from './testData';
 
 async function login(page: Page) {
     await page.goto("https://animated-gingersnap-8cf7f2.netlify.app/");
@@ -22,24 +23,27 @@ async function login(page: Page) {
     await expect(page.getByRole('heading', { name: 'Projects' })).toBeVisible();
 }
 
-test("Implement user authentication is in TODO w/ high priority & feature", async ({ page }) => {
+    for (const taskCase of taskCases) {
+        test(`${taskCase.task} is in ${taskCase.column} w/ tags ${taskCase.tags.join(', ')}`, async ({ page}) => {
+            await login(page);
 
-    await login(page);
+            const project = page.getByRole('button', { name: new RegExp(taskCase.project) });
 
-    const project = page.getByRole('button', { name: /Web Application/ });
-    const toDo = page.getByRole('heading', {name:/To Do/}).locator('..');
-    const task = toDo.getByRole('heading', { name: 'Implement user authentication' }).locator('..');
-    const featureTag = task.getByText('Feature');
-    const highPriorityTag = task.getByText('High Priority');
 
-    await expect(page.getByRole('heading', { name: 'Projects' })).toBeVisible();
+            await expect(project).toBeVisible();
+            await project.click();
 
-    await expect(project).toBeVisible();
-    await project.click();
+            await expect(
+                page.getByRole('banner').getByRole('heading', { name: taskCase.project })).toBeVisible();
 
-    await expect(toDo).toBeVisible();
+            const column = page.getByRole('heading', { name: new RegExp(taskCase.column) }).locator('..');
+            const task = column.getByRole('heading', { name: new RegExp(taskCase.task) }).locator('..');
 
-    await expect(task).toBeVisible();
-    await expect(featureTag).toBeVisible();
-    await expect(highPriorityTag).toBeVisible();
-});
+            await expect(column).toBeVisible();
+            await expect(task).toBeVisible();
+
+                        for (const tag of taskCase.tags) {
+                await expect(task.getByText(tag, { exact: true })).toBeVisible();
+            }
+        });
+    }
